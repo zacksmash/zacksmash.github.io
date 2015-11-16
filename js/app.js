71813b46f57e7ce2bab1c3f1037fd82bf915ceac
zacksmash <zachary.warren@me.com>
2015-11-15 21:59:58 -0700

    Final

--- js/app.js
+++ js/app.js
@@ -1,21 +1,23 @@
 $(document).foundation();
 
-smoothScroll.init({
-  selector: '[data-scroll]', // Selector for links (must be a valid CSS selector)
-  speed: 1000, // Integer. How fast to complete the scroll in milliseconds
-  easing: 'easeInOutQuint', // Easing pattern to use
-  updateURL: false, // Boolean. Whether or not to update the URL with the anchor hash on scroll
-});
-
-$('.current-year').text(new Date().getFullYear());
-
 function flipImage() {
 	$('.card').toggleClass('flipped');
 }
 
-setInterval(function(){ 
-	flipImage()
-}, 5000);
+$(document).ready(function() { 
+	smoothScroll.init({
+	  selector: '[data-scroll]', // Selector for links (must be a valid CSS selector)
+	  speed: 1000, // Integer. How fast to complete the scroll in milliseconds
+	  easing: 'easeInOutQuint', // Easing pattern to use
+	  updateURL: false, // Boolean. Whether or not to update the URL with the anchor hash on scroll
+	});
+	
+	setInterval(function(){ 
+		flipImage()
+	}, 5000);
+	
+	$('.current-year').text(new Date().getFullYear());
+});
 
 /*
 $('.flip').click(function(){
